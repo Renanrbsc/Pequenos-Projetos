20260b2c6f4c3fff67ae6e27ebf4591013b7f6fd
Renan Berti Ribas <renanrbsc@gmail.com>
2020-02-11 03:02:43 -0300

    Modificando diretorio e organizando a pagina web

--- Database_Pokemon/Interface_Terminal_WEB/Views_web/app.py
+++ Database_Pokemon/Interface_Terminal_WEB/Views_web/app.py
@@ -1,10 +1,10 @@
+from flask import Flask, render_template
 import sys
 
 sys.path.append(r'C:\Users\Usuario\Documents\GitHub\Projetos-Testes\Database_Pokemon\Interface_Terminal_WEB')
 
-from flask import Flask, render_template
 
-app = Flask("__name__")
+app = Flask(__name__)
 
 
 @app.route('/')
