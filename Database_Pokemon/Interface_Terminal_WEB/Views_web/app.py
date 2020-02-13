9628855325df1542b89ae58ed2460214e2e5b85c
Renan Berti Ribas <renanrbsc@gmail.com>
2020-02-12 22:16:47 -0300

    Estilizando e menu login para web

--- Database_Pokemon/Interface_Terminal_WEB/Views_web/app.py
+++ Database_Pokemon/Interface_Terminal_WEB/Views_web/app.py
@@ -11,13 +11,23 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    return render_template('index.html')
+
+
+@app.route('/menu')
+def menu_escolha():
+    return render_template('menu_escolha.html')
+
+
+@app.route('/listarpokemon')
+def listar_todos_pokemon():
     poke = PokemonController()
     lista = poke.get_all()
     lista_nome = []
     for i in lista:
         lista_nome.append(i.nome)
 
-    return render_template('index.html', lista = lista_nome)
+    return render_template('Listar_todos_pokemons.html', lista=lista_nome)
 
 
 app.run(debug=True, port=80)
