eff7663f9d5b7f300f5b9592999ec084a130d0ed
Renan Berti Ribas <renanrbsc@gmail.com>
2020-02-16 18:45:15 -0300

    Botoes de editar, excluir e deslogar Admin

--- Database_Pokemon/Interface_Terminal_WEB/Views_web/app.py
+++ Database_Pokemon/Interface_Terminal_WEB/Views_web/app.py
@@ -10,15 +10,27 @@ from Controller_Terminal.administrator_controller import AdministratorController
 app = Flask(__name__)
 login = []
 
+
 @app.route('/')
 def index():
-    return render_template('menu_escolha.html', login = None)
-
+    return render_template('menu_escolha.html', login=None)
 
 
 @app.route('/login')
 def login_admin():
-    return render_template('login.html')
+    Admin_controller = AdministratorController()
+    admin_all = Admin_controller.get_all()
+    username = request.args['username']
+    passwd = request.args['password']
+
+    for admin in admin_all:
+        if str(username).lower() == str(admin.username).lower() \
+                or str(username).lower() == str(admin.email).lower():
+            if passwd == str(admin.password).lower():
+                login.append(username)
+                login.append(passwd)
+                return render_template('menu_admin.html')
+    return redirect('/login')
 
 
 @app.route('/menuadmin')
@@ -26,19 +38,8 @@ def menu_admin():
     if login:
         return render_template('menu_admin.html')
     else:
-        Admin_controller = AdministratorController()
-        admin_all = Admin_controller.get_all()
-        username = request.args['username']
-        passwd = request.args['password']
+        return render_template('login.html')
 
-        for admin in admin_all:
-            if str(username).lower() == str(admin.username).lower() \
-                    or str(username).lower() == str(admin.email).lower():
-                if passwd == str(admin.password).lower():
-                    login.append(username)
-                    login.append(passwd)
-                    return render_template('menu_admin.html')
-        return redirect('/login')
 
 
 @app.route('/menuadminpokemon')
@@ -51,7 +52,23 @@ def menu_admin_pokemon():
     for dados in lista_dados:
         lista.append(dados.list())
 
-    return render_template('menu_admin_pokemon.html', lista = lista)
+    return render_template('menu_admin_pokemon.html', lista=lista)
+
+
+@app.route('/menuadminpokemon/editarpokemon')
+def editar_pokemon():
+    if not login:
+        return redirect('/login')
+    return render_template('menu_admin_pokemon.html')
+
+
+@app.route('/sair')
+def sair_admin():
+    # busca a variavel global para modificar
+    global login
+    login = []
+    return redirect('/')
+
 
 @app.route('/menupokemon')
 def listar_todos_pokemon():
