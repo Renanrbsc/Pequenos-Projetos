52076a9be3fe09baa3bd04b79f3f8032a8a7e1c0
Renan Berti Ribas <renanrbsc@gmail.com>
2020-03-08 18:15:11 -0300

    nao utilizado

--- Locadora_carros/Model/carro.py
+++
@@ -1,9 +0,0 @@
-class Carro:
-	def __init__(self, placa, ano, cor, quilometragem, modelo, marca, registro=None):
-		self.registro = registro
-		self.placa = placa
-		self.ano = ano
-		self.cor = cor
-		self.quilometragem = quilometragem
-		self.modelo = modelo
-		self.marca = marca
